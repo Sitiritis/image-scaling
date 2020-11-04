1799af957bdde972839eff196ae33a45fbd60343
Tymur Lysenko <tymur.lysenko@gmail.com>
2020-11-04 17:45:50 +0300

    Support aspect ratio fitter and made agnostic to orientation and asp rat

--- Assets/Scripts/ParentRectTransformBounder.cs
+++ Assets/Scripts/ParentRectTransformBounder.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// Bounds <c>RectTransform</c> of the current game object to
@@ -8,7 +9,7 @@
 /// </summary>
 public class ParentRectTransformBounder : MonoBehaviour
 {
-    [SerializeField] private RectTransform boundingRectTransform;
+    [SerializeField] public RectTransform boundingRectTransform; // TODO: change to private
     private RectTransform currentRectTransform;
 
     void Awake()
@@ -23,40 +24,83 @@ public class ParentRectTransformBounder : MonoBehaviour
         CenterImage();
     }
 
-    public float MinHorizontalBoundary =>
-        MaxHorizontalBoundary - (
-            (currentRectTransform.rect.width * currentRectTransform.lossyScale.x) -
-            (boundingRectTransform.rect.width * boundingRectTransform.lossyScale.x)
-        );
-    public float MaxHorizontalBoundary =>
-        boundingRectTransform.position.x -
-        (boundingRectTransform.rect.width * boundingRectTransform.lossyScale.x) / 2;
-    public float MinVerticalBoundary =>
-        MaxVerticalBoundary - (
-            (currentRectTransform.rect.height * currentRectTransform.lossyScale.y) -
-            (boundingRectTransform.rect.height * boundingRectTransform.lossyScale.y)
-        );
-    public float MaxVerticalBoundary =>
-        boundingRectTransform.position.y -
-        (boundingRectTransform.rect.height * boundingRectTransform.lossyScale.y) / 2;
+    public float CurrentWidth =>
+        currentRectTransform.rect.width * currentRectTransform.lossyScale.x;
+    public float CurrentHeight =>
+        currentRectTransform.rect.height * currentRectTransform.lossyScale.y;
 
+    public float BoundingHeight =>
+        boundingRectTransform.rect.height * boundingRectTransform.lossyScale.y;
+    public float BoundingWidth =>
+        boundingRectTransform.rect.width * boundingRectTransform.lossyScale.x;
 
-    public Rect BoundaryRect => new Rect(
-        MinHorizontalBoundary,
-        MinVerticalBoundary,
-        currentRectTransform.rect.width * currentRectTransform.lossyScale.x,
-        currentRectTransform.rect.height * currentRectTransform.lossyScale.y
-    );
+    public float MinHorizontalBoundary
+    {
+        get
+        {
+            var boundingWidth = BoundingWidth;
+            var currentWidth = CurrentWidth;
+
+            return MaxHorizontalBoundary - Math.Max(0, currentWidth - boundingWidth);
+        }
+    }
+
+    public float MaxHorizontalBoundary
+    {
+        get
+        {
+            var boundingWidth = BoundingWidth;
+            var currentWidth = CurrentWidth;
+
+            return boundingWidth >= currentWidth
+                ? (currentRectTransform.position.x - (currentWidth / 2))
+                : (boundingRectTransform.position.x - (boundingWidth / 2));
+        }
+    }
+
+    public float MinVerticalBoundary
+    {
+        get
+        {
+            var boundingHeight = BoundingHeight;
+            var currentHeight = CurrentHeight;
+
+            return MaxVerticalBoundary - Math.Max(0, currentHeight - boundingHeight);
+        }
+    }
+
+    public float MaxVerticalBoundary
+    {
+        get
+        {
+            var boundingHeight = BoundingHeight;
+            var currentHeight = CurrentHeight;
+
+            return boundingHeight >= currentHeight
+                ? (currentRectTransform.position.y - (currentHeight / 2))
+                : (boundingRectTransform.position.y - (boundingHeight / 2));
+        }
+    }
+
+    // public Rect BoundaryRect => new Rect(
+    //     MinHorizontalBoundary,
+    //     MinVerticalBoundary,
+    //     CurrentWidth,
+    //     CurrentHeight
+    // );
 
     public Vector2 Position
     {
         get => currentRectTransform.position;
         set
         {
-            var currentMinHorizontalBoundary = MinHorizontalBoundary;
-            var currentMaxHorizontalBoundary = MaxHorizontalBoundary;
-            var currentMinVerticalBoundary = MinVerticalBoundary;
-            var currentMaxVerticalBoundary = MaxVerticalBoundary;
+            var horizontalOffset = CurrentWidth / 2;
+            var verticalOffset = CurrentHeight / 2;
+
+            var currentMinHorizontalBoundary = MinHorizontalBoundary + horizontalOffset;
+            var currentMaxHorizontalBoundary = MaxHorizontalBoundary + horizontalOffset;
+            var currentMinVerticalBoundary = MinVerticalBoundary + verticalOffset;
+            var currentMaxVerticalBoundary = MaxVerticalBoundary + verticalOffset;
 
             var newX = value.x <= currentMinHorizontalBoundary ? currentMinHorizontalBoundary :
                 value.x >= currentMaxHorizontalBoundary ? currentMaxHorizontalBoundary :
@@ -81,16 +125,8 @@ public class ParentRectTransformBounder : MonoBehaviour
         set => Position = new Vector2(Position.x, value);
     }
 
-    public void CenterImage()
-    {
-        var boundingRect = boundingRectTransform.rect;
-        var boundingScale = boundingRectTransform.lossyScale;
-
-        Position = BoundaryRect.center - new Vector2(
-            (boundingRect.width * boundingScale.x) / 2,
-            (boundingRect.height * boundingScale.y) / 2
-        );
-    }
+    public void CenterImage() =>
+        Position = boundingRectTransform.position;
 
     public void SetScale(float scale)
     {
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -13,6 +13,7 @@ public class Test : MonoBehaviour
     void Start()
     {
         Input.gyro.enabled = true;
+        Image.SetScale(2);
     }
 
     private void Update()
@@ -24,5 +25,42 @@ public class Test : MonoBehaviour
         zText.text = $"Z: {currentPosition.z}";
 
         Image.Position += new Vector2(currentPosition.y, -currentPosition.x) * 50;
+
+        var minBoundary = new Vector3(Image.MinHorizontalBoundary, Image.MinVerticalBoundary);
+        var maxBoundary = new Vector3(Image.MaxHorizontalBoundary, Image.MaxHorizontalBoundary);
+        Debug.DrawLine(
+            minBoundary,
+            maxBoundary,
+            Color.green
+        );
+        var posP = PerpendicularLine(minBoundary, maxBoundary);
+        Debug.DrawLine(
+            posP.Item1,
+            posP.Item2,
+            Color.green
+        );
+
+        var boundingMin = Image.boundingRectTransform.position - new Vector3(
+            Image.BoundingWidth / 2,
+            Image.BoundingHeight / 2
+        );
+        var boundingMax = Image.boundingRectTransform.position + new Vector3(
+            Image.BoundingWidth / 2,
+            Image.BoundingHeight / 2
+        );
+        Debug.DrawLine(
+            boundingMin,
+            boundingMax,
+            Color.yellow
+        );
+        var bp = PerpendicularLine(boundingMin, boundingMax);
+        Debug.DrawLine(
+            bp.Item1,
+            bp.Item2,
+            Color.yellow
+        );
     }
+
+    public static (Vector2, Vector2) PerpendicularLine(Vector2 min, Vector2 max) =>
+        (new Vector2(max.x, min.y), new Vector2(min.x, max.y));
 }
